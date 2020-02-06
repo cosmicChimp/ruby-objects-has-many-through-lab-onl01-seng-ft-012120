b46202e470d2cd6f8700807e2fa4a5850366e020
Ian Marrs <ian.t.marrs@gmail.com>
2020-02-06 08:42:45 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -8,6 +8,10 @@ class Patient
     @@all << self
   end
   
+  def appointments
+    Appointment.all.select {|x| x.patient}
+  end
+  
   def new_appointment(doctor, date)
     Appointment.new(date, self, doctor)
   end
